e886798eb8095df2555288765daeb29d30d1d45d
jack231 <j.k.cotrado24@gmail.com>
2017-09-08 20:24:32 -0500

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="hero is-fullheight is-dark is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column is-4 is-offset-4">
+            <h1 class="title">
+             Login
+            </h1>
+            <div class="box">
+              <label class="label">Username</label>
+              <p class="control has-icons-left">
+                <input class="input" type="text" placeholder="usuario" v-model="cred.username">
+                <span class="icon is-small is-left">
+                  <i class="fa fa-user"></i>
+                </span>
+              </p>
+              <label class="label">Password</label>
+              <p class="control has-icons-left">
+                <input class="input" type="password" placeholder="password" v-model="cred.password">
+                <span class="icon is-small is-left">
+                  <i class="fa fa-lock"></i>
+                </span>
+              </p>
+              
+              <hr>
+              <p class="control">
+                <button class="button is-primary" @click="login()">Login</button>
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+
+  name: 'Login',
+
+  data () {
+    return {
+    	cred: {
+    		username: '',
+    		password: ''
+    	}
+    };
+  },
+  methods: {
+  	login(){
+      this.$http.post('/api/usuarios/login', this.cred).then((res, err) => {
+      	console.log(res.data)
+        this.$auth.setToken(res.data.id, res.data.userId, res.data.created, res.data.ttl);
+        this.$router.push('/hello');
+          const loadingComponent = this.$loading.open()
+          setTimeout(() => {
+            loadingComponent.close()
+            }, 0.5 * 1000)
+      });
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
+
--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Auth from './services/Auth.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import Buefy from 'buefy'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+Vue.use(Buefy)
+Vue.use(Auth)
 
 /* eslint-disable no-new */
 new Vue({
