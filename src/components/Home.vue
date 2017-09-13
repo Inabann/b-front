f34edf630779bdb33f5f683234841da8f542359c
anton151 <markuz153@gmail.com>
2017-09-13 11:56:43 -0500

    home

---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Navbar @statusNav="statusMenu = $event"></Navbar> 
+      <div class="columns " style="margin-top: 0px; margin-left: 0px; margin-right: 0px;">
+        <div class="column" id="appmenu" :class="{ 'is-hidden': statusMenu }">
+          <appMenu></appMenu>
+        </div>
+        <div class="column" id="appBody">
+          <div class="container">
+            <router-view></router-view>
+          </div>
+          
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import appMenu from '@/components/Aside'
+
+export default {
+
+  name: 'Home',
+  components: {
+    Navbar,
+    appMenu,
+  },
+  data (){
+    return {
+      statusMenu: false
+    }
+  }
+};
+</script>
+
+<style lang="css">
+.columns:last-child {
+    margin-bottom: 0;
+}
+
+ #appmenu {
+  background-color: #3A3B4A;
+
+ }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,17 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
 import Asesor from '@/components/Asesor/Asesor'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+    },
     {
       path: '/hello',
       name: 'Hello',
@@ -16,7 +22,8 @@ export default new Router({
     {
     	path: '/login',
     	component: Login
-    }, {
+    }, 
+    {
       path: '/asesor',
       name: 'Asesor',
       component: Asesor
