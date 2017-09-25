929a964b9df69bb0e5d4ba519af7fdc66a5999f5
inaban <25hbaam@gmail.com>
2017-09-25 15:22:11 -0500

    detaalle de factura

--- src/components/Factura/Facturas.vue
+++ src/components/Factura/Facturas.vue
@@ -5,7 +5,33 @@
 		<b-modal :active.sync="isComponentModalActive" :canCancel="canCancel" has-modal-card>
       <ModalNuevaFactura></ModalNuevaFactura>
     </b-modal>
+		<b-table
+      :data="facturas"
+      detailed
+      @details-open="(row, index) => detalles(row)">
 
+      <template scope="props">
+        <b-table-column label="Codigo" width="40"  numeric>
+          {{ props.row.codigo }}
+        </b-table-column>
+        <b-table-column label="Fecha de registro" centered>
+          {{ props.row.fecha }}
+        </b-table-column>
+        <b-table-column label="Total">
+          {{ props.row.total }}
+        </b-table-column>
+      </template>
+
+      <template slot="detail" scope="props">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+              {{ detalleFactura }}
+            </div>
+          </div>
+        </article>
+      </template>
+    </b-table>
 	</div>
 </template>
 
@@ -19,8 +45,21 @@ export default {
   data () {
     return {
     	isComponentModalActive: false,
-    	canCancel: ['escape', 'x', 'outside']
+    	canCancel: ['escape', 'x', 'outside'],
+    	facturas: [],
+    	detalleFactura: {}
     };
+  },
+  methods: {
+  	getFacturas(){
+  		this.$http.get('/api/Facturas').then(res => this.facturas = res.data)
+  	},
+  	detalles(row){
+  		this.$http.get('/api/Facturas/'+row.id+'/detalleProductos').then( res => this.detalleFactura = res.data)
+  	}
+  },
+  created(){
+  	this.getFacturas()
   }
 };
 </script>
