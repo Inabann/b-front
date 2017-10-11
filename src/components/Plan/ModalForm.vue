063a41c2c05f548cb12927b5fe97f2a542bdb5df
inaban <25hbaam@gmail.com>
2017-10-11 16:22:59 -0500

    plan 2.0

--- src/components/Plan/ModalForm.vue
+++ src/components/Plan/ModalForm.vue
@@ -1,74 +1,51 @@
 <template>
   <div class="modal-card">
     <header class="modal-card-head">
-      <p class="modal-card-title" v-if="sendPlan">Editar PLan</p>
+      <p class="modal-card-title">Editar PLan</p>
         
     </header>
-    <section class="modal-card-body" v-if="sendPlan">
+    <section class="modal-card-body">
       <b-field grouped>
-        <b-field label="Producto">
-          <b-select placeholder="Producto" icon="user" v-model="plan.productoId">
-            <option v-for="producto in productos" :value="producto.nombre" disabled>{{ producto.nombre }}</option>
-          </b-select>
-        </b-field>
         <b-field label="Plan">
-          <b-input type="text" v-model="plan.nombre" placeholder="Plan"/>
+          <b-input type="text" v-model="sendPlan.nombre" placeholder="Plan"/>
         </b-field>
+        <b-field label="Precio">
+            <b-input type="number" min="0" step=".10" v-model="sendPlan.monto" placeholder="Precio Plan" icon="money" required />
+          </b-field>
       </b-field>
       <div class="columns">
         <div class="column is-4">
-          <b-field label="Precio">
-            <b-input type="number" min="0" step=".10" v-model="plan.monto" placeholder="Precio Plan" icon="money" required />
+          <b-field label="Descuento">
+            <b-input type="number" min="0" step=".10" v-model="sendPlan.descuento" placeholder="Descuento" icon="money" required />
           </b-field>
         </div>
       </div>
     </section>
     <footer class="modal-card-foot ">
       <button class="button" type="button" @click="$parent.close()">Cerrar</button>
-       <button v-show="plan.nombre && plan.monto" class="button is-primary" @click="savePlan">Guardar</button>
+       <button v-show="sendPlan.nombre && sendPlan.monto" class="button is-primary" @click="savePlan">Guardar</button>
     </footer>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['plans','sendPlan'],
+  props: ['sendPlan'],
   data () {
     return {
-      productos: [],
-      plan: {
-        nombre: '',
-        monto: '',
-        productoId: ''
 
-      }
     }
   },
   methods:{ 
-    getProductos(){
-      this.$http.get('/api/Productos').then(res => {
-      this.productos = res.data
-      })
-    },
     savePlan(){
-      this.plan.nombre = this.plan.nombre.toLowerCase()
-      if(this.sendPlan){
-        let index = this.plans.indexOf(this.sendPlan)
-        this.$http.put('/api/Plans/'+this.plan.id, this.plan).then(res => {
-        this.plans[index] = res.data
-        this.$emit('newList', this.plans)
+      this.sendPlan.nombre = this.sendPlan.nombre.toLowerCase()
+      this.$http.put('/api/Plans/'+this.sendPlan.id, this.sendPlan).then(res => {
+        this.$emit('nuevoPlan', res.data)
         this.$parent.close()
         this.$toast.open({message:'Plan editado',type: 'is-success'})
       })
-      }
     }
-  },
-  created: function(){
-    if(this.sendPlan){
-      this.plan = this.sendPlan
-    }
-    this.getProductos()
-  },
+  }
 }
 </script>
 
--- src/components/Plan/Plan.vue
+++ src/components/Plan/Plan.vue
@@ -2,7 +2,7 @@
   <div class="container">
   <br>
   <b-modal :active.sync="isComponentModalActive" has-modal-card :canCancel="canCancel">
-      <ModalForm :plans="plans" :sendPlan="sendPlan" @newList="plans = $event"></ModalForm>
+      <ModalForm :sendPlan="sendPlan" @nuevoPlan="addNuevoPlan($event)"></ModalForm>
   </b-modal>
 
   <section>
@@ -11,20 +11,18 @@
       <div class="content">
         <div class="columns">
           <div class="column is-4">
-            <b-field label="Producto">
-              <b-select placeholder="Producto" icon="list" v-model="plan.productoId">
-                <option v-for="producto in productos" :value="producto.nombre" >{{ producto.nombre }}</option>
-              </b-select>
-            </b-field>  
+            <b-field label="Nombre Plan" expanded>
+              <b-input type="text" v-model="plan.nombre" icon="list" required />
+            </b-field>
           </div>
           <div class="column is-5">
-            <b-field label="Nombre Plan" expanded>
-              <b-autocomplete v-model="plan.nombre" placeholder="Plan" :data="searchPlan" field="nombre" @select="option => nombre = option" required></b-autocomplete>
+            <b-field label="Precio" expanded>
+              <b-input type="number" v-model="plan.monto" placeholder="S/." icon="money" required />
             </b-field>
           </div>
           <div class="column is-3">
-            <b-field label="Precio">
-              <b-input type="number" v-model="plan.monto" placeholder="S/." icon="money" required />
+            <b-field label="Descuento">
+              <b-input type="number" v-model="plan.descuento" placeholder="S/." icon="money" required />
             </b-field>                    
           </div>
 
@@ -42,7 +40,6 @@
     <b-field>
       <b-field grouped>
         <b-input placeholder="plan" type="search" icon-pack="fa" icon="search" v-model="plfilter" class="inputBusqueda"></b-input>
-        <b-input placeholder="producto" type="search" icon-pack="fa" icon="search" v-model="pfilter" class="inputBusqueda"></b-input>
       </b-field>
     </b-field>
 
@@ -51,13 +48,11 @@
         <b-table-column field="nombre" label="Plan" sortable>
           {{ props.row.nombre }}
         </b-table-column>
-        <b-table-column field="productoId" label="Producto" sortable>
-        <div v-if="props.row.producto">
-          {{ props.row.producto.nombre }}
-        </div>
-      </b-table-column>
         <b-table-column field="monto" label="Precio" sortable>
           {{ props.row.monto }}
+        </b-table-column>
+        <b-table-column field="descuento" label="Descuento" sortable>
+          {{ props.row.descuento }}
         </b-table-column>
         <b-table-column  label="Opciones" >
           <a class="button is-warning is-small" @click="editPlan(props.row)">Editar</a>
@@ -83,34 +78,26 @@ export default {
   data () {
     return {
       plans: [],
-      productos: [],
       plan: {
         nombre: '',
         monto: '',
-        productoId: ''
+        descuento: ''
       },
       plfilter: '',
-      pfilter:'',
       sendPlan: {},
       canCancel: ['escape', 'x', 'outside'], //poner esta wea para q no salga el error
       isComponentModalActive: false
     };
   },
   methods:{
-    getProductos(){
-      this.$http.get('/api/Productos').then(res => {
-      this.productos = res.data
-      })
-    },
     getPlans(){
-      this.$http.get('/api/Plans?filter=%7B%22include%22%3A%20%22producto%22%7D').then(res => this.plans = res.data).catch(err => console.log(err))
+      this.$http.get('/api/Plans').then(res => this.plans = res.data).catch(err => console.log(err))
     },
     savePlan(){
       this.plan.nombre = this.plan.nombre.toLowerCase()
       this.$http.post('/api/Plans', this.plan).then(res => {
-      this.plans.unshift(res.data)
-      this.$emit('newList', this.plans)
-      this.$toast.open({message:'Plan guardado',type: 'is-success'})
+        this.plans.unshift(res.data)
+        this.$toast.open({message:'Plan guardado',type: 'is-success'})
       })
     },
     deletePlan(plan){
@@ -133,20 +120,21 @@ export default {
     editPlan(plan){
       this.sendPlan = plan
       this.isComponentModalActive = true
+    },
+    addNuevoPlan(plan){
+      this.plans[this.plans.indexOf(plan)] = plan
     }
   },
   computed:
   {
     searchPlan: function(){
       return this.plans.filter((plan) => {
-        return plan.producto.nombre.match(this.pfilter.toLowerCase()) 
-        && plan.nombre.match(this.plfilter.toLowerCase());
+        return plan.nombre.match(this.plfilter.toLowerCase());
       });
     }
   },
   created: function(){
     this.getPlans();
-    this.getProductos();
   }
 };
 </script>
